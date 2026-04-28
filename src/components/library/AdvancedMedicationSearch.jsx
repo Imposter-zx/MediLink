3280150e6b40563c7bab97c3176ea19d47b94e6d
imposter-zx <alizord4@gmail.com>
2026-04-28 23:36:27 +0100

    Update DoctorDashboard and AdvancedMedicationSearch components

--- src/components/library/AdvancedMedicationSearch.jsx
+++ src/components/library/AdvancedMedicationSearch.jsx
@@ -97,13 +97,9 @@ const applyFilters = () => {
 };
 
 // Apply filters when search or filters change
-const applyFiltersCallback = useCallback(() => {
+useEffect(() => {
   applyFilters();
 }, [searchQuery, filters, medications]);
-
-useEffect(() => {
-  applyFiltersCallback();
-}, [applyFiltersCallback]);
 
   return (
     <div className="max-w-6xl mx-auto p-6">
